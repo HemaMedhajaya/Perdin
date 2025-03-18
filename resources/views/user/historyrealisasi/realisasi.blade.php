29ed7edcaba199504b6e71deada13256d8274687
FrontKurnianuswaruwu <frontment@stramm.co.id>
2025-03-18 20:32:09 +0700

    penyesuaian

--- resources/views/user/historyrealisasi/realisasi.blade.php
+++ resources/views/user/historyrealisasi/realisasi.blade.php
@@ -77,25 +77,6 @@
     </div>
 </div>
 
-<!-- Modal Hapus -->
-<div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="deleteModalLabel">Konfirmasi Hapus</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <p>Apakah Anda yakin ingin menghapus user ini?</p>
-                <input type="hidden" id="deleterealisasi">
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                <button id="confirmDelete" class="btn btn-danger">Hapus</button>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
 
 @section('script')
